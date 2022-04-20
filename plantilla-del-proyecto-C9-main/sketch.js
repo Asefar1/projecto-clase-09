66cfd76d7eae29a9240900d90cb1a6c75ec854a6
Asefar1 <103152215+Asefar1@users.noreply.github.com>
2022-04-20 14:05:31 -0500

    Add files via upload

---
+++ plantilla-del-proyecto-C9-main/sketch.js
@@ -0,0 +1,37 @@
+
+function setup() {
+  createCanvas(400,400);
+  background("yellow");
+  box = createSprite(200,200,30,30);
+
+}
+
+function draw() 
+{
+
+  
+  
+  
+
+  if (keyIsDown(LEFT_ARROW)) 
+  {
+    background("red");
+    
+  }
+ 
+    if (keyIsDown(UP_ARROW)) 
+  {
+    background("yellow");
+   
+  }
+
+  if (keyIsDown(DOWN_ARROW)) 
+  {
+    background("green");
+  }
+
+
+  
+  drawSprites();
+}
+
